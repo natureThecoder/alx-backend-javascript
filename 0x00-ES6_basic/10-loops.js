8ff9acf9b48f6413eceb6c54aee0888dbdb94f84
natureThecoder <basilosinachi1@gmail.com>
2023-07-24 20:34:17 +0000

    task10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const myarray = [];
   for (const idx of array) {
-    myarray.push(appendString + idx);
+    myarray.push(`${appendString}${idx}`);
   }
 
   return myarray;
